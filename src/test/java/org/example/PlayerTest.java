d83f72528343ed91f42584c15b67a238c639c069
tsukinonoroi <seytumerov.e.i21@gmail.com>
2024-02-23 18:41:15 +0300

    дрисня с тестами

--- src/test/java/org/example/PlayerTest.java
+++ src/test/java/org/example/PlayerTest.java
@@ -8,35 +8,30 @@ import java.util.HashMap;
 public class PlayerTest {
 
     private static ArrayList<Player> players;
-    private static HashMap<String,Piece> pieceHashMap;
 
     @BeforeAll
     public static void setUp() {
         ChessMain main = new ChessMain();
         players = main.createPlayers();
-        pieceHashMap = main.createPieces();
-
-    }
-
-    @BeforeEach
-    public void beforeEatchTest() {
-        System.out.println("Running before each test");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        System.out.println("Running after each test");
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        System.out.println("Running after all test");
     }
 
     @Test
-    public void validMove() {
-        players.get(0).movePiece(pieceHashMap.get("white_king"), new Spot ("h",8));
+    public void validEmail() {
+        if (players.get(0).getEmail().contains("@") && players.get(0).getEmail().contains(".")) {
+            System.out.println("All ok email is valid");
+        } else {
+            System.out.println("invalid email");
+        }
+    }
 
-        Assertions.assertEquals(8, pieceHashMap.get("white_king").getSpot().getY());
+    @Test
+    public void validName() {
+        if (players.get(0).getName() == null || players.get(0).getName().isEmpty()) {
+            System.out.println("invalid name lox");
+        }
+        else {
+            System.out.println("All ok name is valid");
+        }
     }
 }
+
